df0c324a3011b7f56bb0a61bcb38fdc6d6bd7c91
carola123321 <pazamerica789@hotmail.com>
2026-07-03 21:52:39 +0200

    Add configuration for bot token and chat ID

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+CHAT_ID = os.getenv("CHAT_ID", "")
+URL = "https://sede.administracionespublicas.gob.es/icpplustiej/citar?org=JUS-RC&locale=es&appVersion=V+7.44.4"
