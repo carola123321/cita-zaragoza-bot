a6efe18e1b98c0605fd62c9ea742781932f77ea4
carola123321 <pazamerica789@hotmail.com>
2026-07-03 22:09:34 +0200

    Implement async appointment checking and notification

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("JuraAlert iniciado")
+import asyncio
+
+from monitor import comprobar_citas
+from telegram_bot import enviar_aviso
+
+
+async def main():
+
+    print("Comprobando citas...")
+
+    disponible = await comprobar_citas()
+
+    if disponible:
+        await enviar_aviso(
+            "🚨🚨🚨 ¡¡HAY UNA CITA DISPONIBLE EN ZARAGOZA!! 🚨🚨🚨"
+        )
+    else:
+        print("Todavía no hay citas.")
+
+
+asyncio.run(main())
