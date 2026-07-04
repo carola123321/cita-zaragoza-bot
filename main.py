188f629e42961b8cc3c009ea2ffaac906c9a3f9e
carola123321 <pazamerica789@hotmail.com>
2026-07-04 14:18:10 +0200

    Refactor aviso sending in main.py

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 import asyncio
-
 from monitor import comprobar_citas
 from telegram_bot import enviar_aviso
 
-
 async def main():
-
     print("Comprobando citas...")
 
     disponible = await comprobar_citas()
 
     if disponible:
-        await enviar_aviso(
-            "🚨🚨🚨 ¡¡HAY UNA CITA DISPONIBLE EN ZARAGOZA!! 🚨🚨🚨"
-        )
+        await enviar_aviso("🚨🚨🚨 ¡HAY UNA CITA DISPONIBLE EN ZARAGOZA! 🚨🚨🚨")
     else:
         print("Todavía no hay citas.")
 
-
 asyncio.run(main())
-from datos import BOT_TOKEN, CHAT_ID, NIE, NOMBRE
-
-print(BOT_TOKEN)
-print(NIE)
-print(NOMBRE)
